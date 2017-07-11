eba59333f4f9b911f9ce5fdf41901a983b4c33e6
Michael Bashford <mikebashford@gmail.com>
2017-07-11 14:56:21 +0000

    Fixed a syntax error in the deliver method

--- config/environment.rb
+++ config/environment.rb
@@ -13,4 +13,4 @@ ActionMailer::Base.smtp_settings =
   :domain         => 'aqueous-anchorage-24476.herokuapp.com',
   :authentication => :plain,
 }
-ActionMailer::Base.deliver_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
