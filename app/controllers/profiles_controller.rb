5958e980c2518e6f52c86d25b1466504bc14a57d
Michael Bashford <mikebashford@gmail.com>
2017-07-14 02:35:59 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,14 @@
 class ProfilesController < ApplicationController
-  #GET to /users/:user_id/profile/new
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
     @profile = Profile.new
   end
   
-  #POST to /users/:user_id/profile
+  # POST to /users/:user_id/profile
   def create
     # Ensure that we have the user wo is filling out form
     @user =User.find(params[:user_id])
@@ -44,5 +47,10 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
