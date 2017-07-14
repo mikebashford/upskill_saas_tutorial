bc2e3d78074fa285d71c54f98c6e836056a05399
Michael Bashford <mikebashford@gmail.com>
2017-07-14 00:55:21 +0000

    Added avatar column to Profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714002607) do
+ActiveRecord::Schema.define(version: 20170714004348) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
